40beed4ee2b8e52865a743b67e5bf44d2dd16d69
Rafał Szyiński <72799471+Selvan66@users.noreply.github.com>
2021-09-28 17:07:51 +0200

    match template

--- main.py
+++ main.py
@@ -9,14 +9,16 @@ from pynput.keyboard import Listener, Key
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
+############################OPTIONS###########################
+
 delay = 0.001
-button = Button.left
 start_stop_key = Key.caps_lock
 exit_key = Key.esc
+
+##############################################################
+
+button = Button.left
 mouse = Controller()
-pos_cookie_key = Key.f1
-neg_cookie_key = Key.f2
-pos_golden_key = Key.f3
 
 class Bot(threading.Thread):
 	def __init__(self):
@@ -39,9 +41,10 @@ class Bot(threading.Thread):
 		self.image = cv2.cvtColor(np.array(self.image),cv2.COLOR_RGB2BGR)
 
 	def find_on_screen(self, photo):
+		self.make_screeshot()	
 		find_photo = cv2.imread(photo, cv2.IMREAD_UNCHANGED)
 		result = cv2.matchTemplate(self.image, find_photo, cv2.TM_CCOEFF_NORMED)
-		min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+		_, max_val, _, max_loc = cv2.minMaxLoc(result)
 		threshold = 0.7
 		if max_val >= threshold:
 			cookie_width = find_photo.shape[1]
@@ -49,9 +52,7 @@ class Bot(threading.Thread):
 
 			top_left = max_loc
 			middle = (top_left[0] + (cookie_width / 2), top_left[1] + (cookie_height / 2))
-			self.mouse_pos = middle	
-		cv2.imshow('test', result)
-		cv2.waitKey(100)	
+			self.mouse_pos = middle		
 
 	def exit(self):
 		self.stop_clicking()
@@ -59,10 +60,7 @@ class Bot(threading.Thread):
 
 	def run(self):
 		while self.program_running:
-			self.make_screeshot()
-			#self.find_on_screen('Photo/main_cookie.jpg')
-			self.find_on_screen('Photo/wrath_cookie.jpg')
-			self.find_on_screen('Photo/golden_cookie.jpg')
+			self.find_on_screen('Photo/main_cookie.jpg')
 			while self.running:
 				mouse.position = self.mouse_pos
 				mouse.click(self.button)
