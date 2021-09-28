4859eb7706e99cb1613ea9d909d0bb15f4113b18
Rafał Szyiński <72799471+Selvan66@users.noreply.github.com>
2021-09-28 14:10:32 +0200

    file deleting

--- negfile.py
+++
@@ -1,6 +0,0 @@
-import os
-
-def make_neg_file():
-    with open('negWorm.txt', 'w') as f:
-        for filename in os.listdir('HaarCascade/Worm/Negative'):
-            f.write('HaarCascade/Worm/Negative/' + filename + '\n')
