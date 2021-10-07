0ec42266aa245c3a311421df418af95a4c465c88
Rafał Szyiński <72799471+Selvan66@users.noreply.github.com>
2021-10-07 15:50:25 +0200

    windowcapture v3

--- windowcapture.py
+++ windowcapture.py
@@ -1,6 +1,7 @@
 from PIL import ImageGrab
 import numpy as np
-import win32gui
+import time
+import win32gui, win32ui
 import cv2
 
 class CookieWindowCapture:
@@ -11,23 +12,45 @@ class CookieWindowCapture:
     w, h = 0, 0
 
     def __init__(self):
-        self.find_window()
-        self.win_rec = win32gui.GetWindowRect(self.hwnd)
-        self.w = self.win_rec[2] - self.win_rec[0]
-        self.h = self.win_rec[3] - self.win_rec[1]
+        self.find_window()    
 
     def find_window(self):
         self.get_winlist()
         self.cookiewin =  [(hwnd, title) for hwnd, title in self.winlist if 'cookie clicker' in title.lower()]
+        while not self.cookiewin:
+            print('Cannot find cookie cliker window')
+            self.get_winlist()
+            self.cookiewin =  [(hwnd, title) for hwnd, title in self.winlist if 'cookie clicker' in title.lower()]
+            time.sleep(1)
+
         self.cookiewin = self.cookiewin[0]
         self.hwnd = self.cookiewin[0]
+        self.win_rec = win32gui.GetWindowRect(self.hwnd)
+        self.w = self.win_rec[2] - self.win_rec[0]
+        self.h = self.win_rec[3] - self.win_rec[1]
 
     def get_screen(self):
         self.find_window()
-        win32gui.SetForegroundWindow(self.hwnd)
-        img = ImageGrab.grab(self.win_rec)
-        img = np.array(img)
-        img = cv2.cvtColor(img, cv2.COLOR_RGBA2BGR)
+        #win32gui.SetForegroundWindow(self.hwnd)
+        wDC = win32gui.GetWindowDC(self.hwnd)
+        dcObj = win32ui.CreateDCFromHandle(wDC)
+        cDC = dcObj.CreateCompatibleDC()
+        dataBitMap = win32ui.CreateBitmap()
+        dataBitMap.CreateCompatibleBitmap(dcObj, self.w, self.h)
+        #cDC.SelectObject(dataBitMap)
+        #cDC.BitBlt((0, 0), (self.w, self.h), dcObj, (self.cropped_x, self.cropped_y), win32con.SRCCOPY)
+        signedIntsArray = dataBitMap.GetBitmapBits(True)
+        img = np.fromstring(signedIntsArray, dtype='uint8')
+        img.shape = (self.h, self.w, 4)
+
+        # free resources
+        dcObj.DeleteDC()
+        cDC.DeleteDC()
+        win32gui.ReleaseDC(self.hwnd, wDC)
+        win32gui.DeleteObject(dataBitMap.GetHandle())
+        img = img[...,:3]
+        img = np.ascontiguousarray(img)
+
         return img
 
     def get_winlist(self):
@@ -37,3 +60,10 @@ class CookieWindowCapture:
 
     def get_pos(self, pos):
         return (pos[0] + self.win_rec[0], pos[1] + self.win_rec[1])
+
+if __name__ == '__main__':
+    try:
+        w = CookieWindowCapture()
+        w.get_screen()
+    except:
+        print('test')
